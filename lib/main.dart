a92f415055d439f0635ddb92f4cb12cf6f46b1ed
Bezes13 <AlexBelzer98@gmail.com>
2024-05-26 22:17:48 +0200

    darmode

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:remindme/ChipSelection.dart';
@@ -19,25 +20,45 @@ main() {
 // 4 Animated Container for chips?
 // 2Launcher icon
 // interactive viewer
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+
+  static _MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>()!;
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.system;
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Remind Me',
-          theme: ThemeData(
-            useMaterial3: true,
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.pinkAccent),
+        debugShowCheckedModeBanner: false,
+        title: 'Remind Me',
+        darkTheme: ThemeData.dark().copyWith(
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+            backgroundColor: Colors.pinkAccent,
           ),
+        ),
+        theme: ThemeData(useMaterial3: true, colorScheme: ColorScheme.fromSeed(seedColor: Colors.pinkAccent),),
+        themeMode: _themeMode,
         routes: {
           '/': (context) => MyHomePage(),
           '/another': (context) => CreationPage(),
-        },),
+        },
+      ),
     );
+  }
+
+  void changeTheme(ThemeMode themeMode) {
+    setState(() {
+      _themeMode = themeMode;
+    });
   }
 }
 
@@ -57,20 +78,20 @@ class _EntryListScreen extends StatefulWidget {
 
 class _EntryListScreenState extends State<_EntryListScreen> {
   final picker = ImagePicker();
-  TextEditingController _searchController = TextEditingController();
-
+  bool isDarkMode = false;
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       Provider.of<MyAppState>(context, listen: false).loadEntries();
     });
+    isDarkMode = Theme.of(context).colorScheme.brightness == Brightness.dark;
   }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final itemStyle = theme.textTheme.displayMedium!.copyWith(color: CupertinoColors.activeBlue);
+    final itemStyle = theme.textTheme.displayMedium!;
     var appState = Provider.of<MyAppState>(context);
 
     return Scaffold(
@@ -78,41 +99,83 @@ class _EntryListScreenState extends State<_EntryListScreen> {
         slivers: [
           SliverAppBar(
               leading: SvgPicture.asset(
-                'lib/Logo.svg',
-                semanticsLabel: 'My SVG Image',
+                'lib/download.svg',
+                semanticsLabel: '',
                 height: 100,
                 width: 70,
+                color: Colors.pinkAccent,
               ),
-              foregroundColor: Colors.pinkAccent.shade100,
               forceElevated: true,
               pinned: true,
               title: Center(
-            child: RichText(
-                    text: TextSpan(
-            text: 'Re',
-            style: DefaultTextStyle.of(context).style.copyWith(color: Colors.pinkAccent, decoration: TextDecoration.none),
-            children: const <TextSpan>[
-              TextSpan(text: 'My', style: TextStyle(fontWeight: FontWeight.bold, color: CupertinoColors.activeBlue)),
-            ],
-                    ),
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Re',
+                    style: DefaultTextStyle.of(context).style.copyWith(
+                        color: Colors.pinkAccent,
+                        decoration: TextDecoration.none),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: 'My',
+                          style: DefaultTextStyle.of(context).style.copyWith(
+                              color: (Theme.of(context).textTheme.bodyLarge?.color),
+                              decoration: TextDecoration.none))
+
+                    ],
                   ),
-          )),
+                ),
+              )),
           SliverToBoxAdapter(
             child: Column(
               children: [
                 Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    controller: _searchController,
-                    onChanged: (value) {
-                      appState.filter(value);
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'Search...',
-                      border: UnderlineInputBorder(),
-                    ),
-                  ),
-                ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: SearchAnchor(builder:
+                        (BuildContext context, SearchController controller) {
+                      return SearchBar(
+                        controller: controller,
+                        padding: const MaterialStatePropertyAll<EdgeInsets>(
+                            EdgeInsets.symmetric(horizontal: 16.0)),
+                        onChanged: (filter) {
+                          appState.filter(filter);
+                        },
+                        leading: const Icon(Icons.search),
+                        trailing: <Widget>[
+                          Tooltip(
+                            message: 'Change brightness mode',
+                            child: IconButton(
+                              isSelected: isDarkMode,
+                              onPressed: () {
+                                setState(() {
+                                  isDarkMode = !isDarkMode;
+                                  if (isDarkMode) {
+                                    MyApp.of(context).changeTheme(ThemeMode.dark);
+                                  } else {
+                                    MyApp.of(context).changeTheme(ThemeMode.light);
+                                  }
+                                });
+                              },
+                              icon: const Icon(Icons.wb_sunny_outlined),
+                              selectedIcon:
+                                  const Icon(Icons.brightness_2_outlined),
+                            ),
+                          )
+                        ],
+                      );
+                    }, suggestionsBuilder:
+                        (BuildContext context, SearchController controller) {
+                      return List<ListTile>.generate(5, (int index) {
+                        final String item = 'item $index';
+                        return ListTile(
+                          title: Text(item),
+                          onTap: () {
+                            setState(() {
+                              controller.closeView(item);
+                            });
+                          },
+                        );
+                      });
+                    })),
                 ChipSelection(
                   currentTag: "",
                   onSelected: appState.filterByTag,
@@ -123,7 +186,7 @@ class _EntryListScreenState extends State<_EntryListScreen> {
           ),
           SliverList(
             delegate: SliverChildBuilderDelegate(
-                  (context, index) {
+              (context, index) {
                 return Card(
                   child: ListTile(
                     title: Text(
@@ -151,13 +214,14 @@ class _EntryListScreenState extends State<_EntryListScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.pushNamed(context, "/another", arguments: Entry("", "", [], ""));
+          Navigator.pushNamed(context, "/another",
+              arguments: Entry("", "", [], ""));
         },
         tooltip: 'Add Entry',
         child: Icon(Icons.add),
+        //backgroundColor: appState.darkMode ? Colors.pinkAccent : Theme.of(context).floatingActionButtonTheme.backgroundColor ,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
-
   }
 }
